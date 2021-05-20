91f3a70e6d5ff2e4ba47e8df9b0e12b0987ba24a
Matt Fysh <mattfysh@gmail.com>
2021-05-20 15:00:44 +1000

    update bug logging

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -2,7 +2,7 @@ import { add } from '../src/add'
 import d from '../src/add'
 import * as e from '../src/add'
 
-console.log({ d, e})
+console.log({ add, d, e })
 
 test('1+1', () => {
   expect(add(1, 1)).toEqual(2)
